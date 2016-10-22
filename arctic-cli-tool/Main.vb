cc78fd6cb7068535a565e440c4e2cda774d0d16d
giancarlo <giancarlo.villena.delacruz@gmail.com>
2016-10-22 07:48:24 -0500

    last commit

---
+++ arctic-cli-tool/Main.vb
@@ -0,0 +1,135 @@
+﻿Imports System.Text
+Imports CommandLine
+Imports CommandLine.Text
+Imports Newtonsoft.Json
+
+Module Main
+
+    Sub Main()
+
+        ' Declare and initialize parser 
+        Dim parser = New Parser(Sub(config) config.HelpWriter = Console.Out)
+
+        ' Parse result
+        Dim args = My.Application.CommandLineArgs
+        Dim result = parser.ParseArguments(Of RunOptions, ListOptions, GradeOptions, TestOptions)(args)
+
+        ' Execute parsed action
+        result.WithParsed(Of RunOptions)(AddressOf Run).
+                WithParsed(Of ListOptions)(AddressOf List).
+                WithParsed(Of GradeOptions)(AddressOf Grade).
+                WithParsed(Of TestOptions)(AddressOf Test).
+                WithNotParsed(Function(errors) 1)
+
+    End Sub
+
+    Function Run(opts As RunOptions)
+        Return 0
+    End Function
+
+    Function List(opts As ListOptions)
+
+        Dim sb = New StringBuilder()
+        Dim stdLst = StudentIO.GetStudents("sim162-lst.txt")
+
+        sb.AppendLine()
+        sb.AppendLine(" " & HeadingInfo.Default.ToString)
+        sb.AppendLine()
+
+        sb.AppendFormat("  {0,-10}  {1, -40} {2}", "Id", "Name", vbCrLf)
+
+        For Each std As Student In stdLst
+            sb.AppendFormat("  {0,-10}  {1, -40} {2}", std.Id, std.Name, vbCrLf)
+        Next
+
+        Console.WriteLine()
+        Console.WriteLine(sb.ToString())
+        Return 0
+    End Function
+
+    Function Grade(opts As GradeOptions)
+
+        Console.WriteLine()
+        Console.WriteLine("Calculate Grade Test v0.2")
+        Console.WriteLine("-------------------------")
+        Console.WriteLine()
+
+        Console.Write("Codigo Alumno: ")
+        Dim id = Console.ReadLine()
+
+        Dim lstFileName = "sim162-lst.txt"
+        Dim std = StudentIO.GetStudent(id, lstFileName)
+        If std Is Nothing Then
+            Console.WriteLine()
+            Console.WriteLine("Opss :( no pude encontrar el codigo " & id & " en la lista " & lstFileName)
+            Console.WriteLine("Verifica que todo este bien e intentalo de nuevo... ;)")
+            Return 0
+        End If
+
+        Console.WriteLine(std.Name & Environment.NewLine)
+
+        Dim grde = StudentIO.CalcGrade(Course.sim162, Evaluation.P1)
+        Console.WriteLine()
+        Console.WriteLine("¡Finalizado!")
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine("Nota P1: " & grde.ToString("#0.00"))
+
+        If grde > 10.5 Then
+            Console.WriteLine("Aprobado ;)")
+        Else
+
+            Dim msgs = New ArrayList()
+            With msgs
+                .Add("Lo importante es que tienes salud ;)")
+                .Add("Falta poco para verano ;)")
+                .Add("¡A nada! ;)")
+                .Add("¡Susti! ;)")
+            End With
+
+            Console.WriteLine("Sorry :(")
+            Console.WriteLine(msgs(New Random().Next(3)))
+
+        End If
+
+
+        Return 0
+    End Function
+
+    Function Test(opts As TestOptions)
+
+        Console.WriteLine()
+        Console.WriteLine("¡Hello World!")
+
+        'Dim sb = New StringBuilder()
+        'Dim stdLst = StudentIO.GetStudents("sim162-lst.txt")
+
+        'sb.AppendLine()
+        'sb.AppendLine("  " & HeadingInfo.Default.ToString)
+        'sb.AppendLine()
+
+        'sb.AppendFormat("  {0,-10}  {1, -40} P1  EP  P2  EF  PF {2}", "Id", "Name", vbCrLf)
+
+        'For Each std As Student In stdLst
+        '    sb.AppendFormat("  {0,-10} {1, -40} {2:00} {}", std.Id, std.Name,
+        '                                                    std.Grades(Evaluation.P1),
+        '                                                    std.Grades(Evaluation.EP),
+        '                                                    std.Grades(Evaluation.P2),
+        '                                                    std.Grades(Evaluation.EF),
+        '                                                    std.FinalGrade,
+        '                                                    vbCrLf)
+        'Next
+
+        'Console.WriteLine()
+        'Console.WriteLine(sb.ToString())
+
+        'Dim stdLst = StudentIO.GetStudents("visual162-lst.txt")
+        'StudentIO.SetGrades("sim162-lst.txt", "prac01.txt")
+        'Dim stdLst = StudentIO.GetGrades("visual162-lst.txt")
+        'Dim json = JsonConvert.SerializeObject(stdLst, Formatting.Indented)
+        'Console.WriteLine(json)
+        Return 0
+
+    End Function
+
+End Module
---
+++ arctic-cli-tool/StudentIO.vb
@@ -0,0 +1,312 @@
+﻿Imports System.Text
+
+Public Class StudentIO
+
+    Public Shared Function GetStudent(id As String, fileName As String) As Student
+
+        Dim std As Student = Nothing
+
+        Dim path As String = IO.Path.Combine(My.Application.Info.DirectoryPath, fileName)
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(path)
+
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(vbTab)
+
+            Dim currentRow As String()
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    If id = currentRow.ElementAt(1) Then
+                        std = New Student()
+                        With std
+                            .Id = currentRow.ElementAt(1)
+                            .Name = currentRow.ElementAt(2)
+                            .Grades.Add(Evaluation.P1, 0)
+                            .Grades.Add(Evaluation.EP, 0)
+                            .Grades.Add(Evaluation.P2, 0)
+                            .Grades.Add(Evaluation.EF, 0)
+                        End With
+                        Exit While
+                    End If
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    Console.WriteLine("Line " & ex.Message & "is not valid and will be skipped.")
+                End Try
+            End While
+        End Using
+
+        Return std
+    End Function
+
+    Public Shared Function GetStudents(ByVal fileName As String) As ArrayList
+
+        Dim lst As ArrayList = New ArrayList()
+        Dim std As Student = Nothing
+        Dim path As String = IO.Path.Combine(My.Application.Info.DirectoryPath, fileName)
+
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(path)
+
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(vbTab)
+
+            Dim currentRow As String()
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    std = New Student()
+                    With std
+                        .Id = currentRow.ElementAt(1)
+                        .Name = currentRow.ElementAt(2)
+                        .Grades.Add(Evaluation.P1, 0)
+                        .Grades.Add(Evaluation.EP, 0)
+                        .Grades.Add(Evaluation.P2, 0)
+                        .Grades.Add(Evaluation.EF, 0)
+                    End With
+                    lst.Add(std)
+                Catch ex As Microsoft.VisualBasic.
+                            FileIO.MalformedLineException
+                    Console.WriteLine("Line " & ex.Message &
+                    "is not valid and will be skipped.")
+                End Try
+            End While
+        End Using
+
+        'For Each s As Student In lst
+        '    Console.WriteLine("{0} {1}", CStr(s.Id), s.Name)
+        'Next
+        Return lst
+    End Function
+
+    Public Shared Sub SetGrades(stdsFileName As String, testFileName As String)
+
+        Console.WriteLine()
+        Console.Write("Codigo Alumno: ")
+        Dim id = Console.ReadLine()
+
+        Dim std = GetStudent(id, stdsFileName)
+
+        If std Is Nothing Then
+            Console.WriteLine()
+            Console.WriteLine("Opss :( no pude encontrar el codigo " & id & " en la lista " & stdsFileName)
+            Console.WriteLine("Verifica que todo este bien e intentalo de nuevo... ;)")
+            Exit Sub
+        End If
+
+        Console.WriteLine(std.Name & Environment.NewLine)
+        'Console.WriteLine("Ingrese...")
+
+        Dim sb = New StringBuilder()
+        sb.Append(id & ControlChars.Tab)
+
+        Dim pdata = GetQuestionData(testFileName)
+
+        For i = 0 To 3
+            Console.Write("Respuesta " & pdata(i).Name & " ")
+            Dim respStr = Console.ReadLine()
+            sb.Append(respStr & ControlChars.Tab)
+            Console.Write(ControlChars.Cr)
+            'Dim resp = Convert.ToInt32(respStr, 2)
+            'Console.WriteLine(resp)
+        Next
+
+        Dim path = IO.Path.Combine(My.Application.Info.DirectoryPath, testFileName)
+        IO.File.AppendAllText(path, sb.ToString & Environment.NewLine)
+        Console.WriteLine(sb.ToString)
+        'For Each preg In pdata
+        'Next
+
+    End Sub
+
+
+    Public Shared Function GetGrades(ByVal fileName As String) As ArrayList
+
+        Dim lst As ArrayList = New ArrayList()
+        Dim std As Student = Nothing
+        Dim path As String = IO.Path.Combine(My.Application.Info.DirectoryPath, fileName)
+
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(path)
+
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(vbTab)
+
+            Dim currentRow As String()
+
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    std = New Student()
+                    With std
+                        .Id = currentRow.ElementAt(1)
+                        .Name = currentRow.ElementAt(2)
+                        .Grades.Add(Evaluation.P1, CalcGrade("2007202383", "prac01.txt"))
+                        .Grades.Add(Evaluation.EP, 0)
+                        .Grades.Add(Evaluation.P2, 0)
+                        .Grades.Add(Evaluation.EF, 0)
+                        '.Grades.Add(Evaluacion.EP, CalcGrade("2007202389", "exprcl.txt"))
+                        '.Grades.Add(Evaluacion.P2, CalcGrade("2007202389", "prac02.txt"))
+                        '.Grades.Add(Evaluacion.EF, CalcGrade("2007202389", "exfinl.txt"))
+                    End With
+                    lst.Add(std)
+                Catch ex As Microsoft.VisualBasic.
+                            FileIO.MalformedLineException
+                    Console.WriteLine("Line " & ex.Message &
+                    "Is Not valid And will be skipped.")
+                End Try
+            End While
+        End Using
+
+        'For Each preg In pregList
+        '    Console.WriteLine(preg01.Name & "" & preg01.Score)
+        'Next
+
+        'For Each s As Student In lst
+        '    Console.WriteLine("{0} {1}", CStr(s.Id), s.Name)
+        'Next
+        Return lst
+
+    End Function
+
+    Public Shared Function CalcGrade(course As Course, eval As Evaluation) As Double
+
+        Dim grade As Double = 0
+
+        Select Case course
+
+            Case Course.sim162 ' TODO CalcGrade sim162
+
+                Console.WriteLine("Ingrese sus respuestas... ")
+                Console.WriteLine()
+
+                Dim pdata = GetQuestionData("prac01.txt")
+                For Each preg In pdata
+                    Console.Write(" Respuesta " & preg.Name & ": ")
+                    Dim ansStr = Console.ReadLine()
+                    Dim ans = Convert.ToInt32(ansStr, 2)
+                    grade += IIf(ans = preg.Answer, preg.Score, 0)
+                    Console.Write(ControlChars.Cr)
+                Next
+
+            Case Course.pweb162 ' TODO CalcGrade pweb162
+            Case Course.so162 ' TODO CalcGrade so162
+            Case Course.visual162 ' TODO CalcGrade visual162
+        End Select
+
+        Return grade
+
+    End Function
+
+
+
+    Private Shared Function CalcGrade(id As String, fileName As String) As Double
+
+        Dim grade As Double = 0
+        Dim path As String = IO.Path.Combine(My.Application.Info.DirectoryPath, fileName)
+
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(path)
+
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(vbTab)
+
+            Dim currentRow As String()
+
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+
+                    If id = currentRow.ElementAt(0) Then
+
+                        Dim pdata = GetQuestionData(fileName)
+                        For Each preg In pdata
+                            Dim stdAnswer = Convert.ToInt32(currentRow.ElementAt(preg.Num), 2)
+                            grade += IIf(stdAnswer = preg.Answer, preg.Score, 0)
+                        Next
+
+                    End If
+
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    Console.WriteLine("Line " & ex.Message & "Is Not valid And will be skipped.")
+                End Try
+            End While
+        End Using
+        Console.WriteLine("Id: " & id & " Grade: " & grade)
+        Return grade
+    End Function
+
+    Public Shared Function GetQuestionData(fileName As String) As ArrayList
+
+        Dim pdata As ArrayList = Nothing
+
+        Select Case fileName
+
+            Case "prac01.txt"
+                ' TODO prac01.txt
+                ' TODO prac01.txt schema
+                ' TODO prac01.txt modificaciones preguntas compuestas
+                Dim p1 = New With {Key .Name = "P1", .Answer = Alternativa.B, .Score = 0.5, .Num = 1}
+
+                Dim p2_1 = New With {Key .Name = "P2.1", .Answer = Alternativa.C, .Score = 0.25, .Num = 2}
+                Dim p2_2 = New With {Key .Name = "P2.2", .Answer = Alternativa.Proposito, .Score = 0.25, .Num = 3}
+
+                Dim p3_1 = New With {Key .Name = "P3.1", .Answer = Alternativa.D, .Score = 0.25, .Num = 4}
+                Dim p3_2 = New With {Key .Name = "P3.2", .Answer = Alternativa.Ventaja, .Score = 0.25, .Num = 5}
+
+                Dim p4_1 = New With {Key .Name = "P4.1", .Answer = Alternativa.D, .Score = 0.25, .Num = 6}
+                Dim p4_2 = New With {Key .Name = "P4.2", .Answer = Alternativa.Desventaja, .Score = 0.25, .Num = 7}
+
+                Dim p5_1 = New With {Key .Name = "P5.1", .Answer = Alternativa.NoAnswer, .Score = 0.25, .Num = 8}
+                Dim p5_2 = New With {Key .Name = "P5.2", .Answer = Alternativa.NoAnswer, .Score = 0.25, .Num = 9}
+
+                Dim p6_1 = New With {Key .Name = "P6.1", .Answer = Alternativa.C, .Score = 0.25, .Num = 10}
+                Dim p6_2 = New With {Key .Name = "P6.2", .Answer = Alternativa.OtrasCons, .Score = 0.25, .Num = 11}
+
+                Dim p7_1 = New With {Key .Name = "P7.1", .Answer = Alternativa.D, .Score = 0.25, .Num = 12}
+                Dim p7_2 = New With {Key .Name = "P7.2", .Answer = Alternativa.Proposito, .Score = 0.25, .Num = 13}
+
+                Dim p8 = New With {Key .Name = "P8", .Answer = Alternativa.A, .Score = 0.5, .Num = 14}
+                Dim p9 = New With {Key .Name = "P9", .Answer = Alternativa.C, .Score = 0.5, .Num = 15}
+
+                Dim p10_1 = New With {Key .Name = "P10.1", .Answer = Alternativa.B, .Score = 0.5, .Num = 16}
+                Dim p10_2 = New With {Key .Name = "P10.2", .Answer = Alternativa.Entidad, .Score = 0.5, .Num = 17}
+
+                Dim p11_1 = New With {Key .Name = "P11.1", .Answer = Alternativa.NoAnswer, .Score = 0.5, .Num = 18}
+                Dim p11_2 = New With {Key .Name = "P11.2", .Answer = Alternativa.NoAnswer, .Score = 0.5, .Num = 19}
+
+                Dim p12_1 = New With {Key .Name = "P12.1", .Answer = Alternativa.A, .Score = 0.5, .Num = 20}
+                Dim p12_2 = New With {Key .Name = "P12.2", .Answer = Alternativa.Recurso, .Score = 0.5, .Num = 21}
+
+                Dim p13_1 = New With {Key .Name = "P13.1", .Answer = Alternativa.C, .Score = 0.5, .Num = 22}
+                Dim p13_2 = New With {Key .Name = "P13.2", .Answer = Alternativa.Recurso, .Score = 0.5, .Num = 23}
+
+                Dim p14 = New With {Key .Name = "P14", .Answer = Alternativa.A And Alternativa.C, .Score = 1, .Num = 24}
+
+                pdata = New ArrayList({p1,
+                                        p2_1, p2_2,
+                                        p3_1, p3_2,
+                                        p4_1, p4_2,
+                                        p5_1, p5_2,
+                                        p6_1, p6_2,
+                                        p7_1, p7_2,
+                                        p8, p9,
+                                        p10_1, p10_2,
+                                        p11_1, p11_2,
+                                        p12_1, p12_2,
+                                        p13_1, p13_2,
+                                        p14})
+
+            Case "exprcl.txt"
+                ' TODO exprcl.txt
+                ' TODO exprcl.txt schema
+            Case "prac02.txt"
+                ' TODO prac02.txt
+                ' TODO prac02.txt schema
+            Case "exfinl.txt"
+                ' TODO exfinl.txt
+                ' TODO exfinl.txt schema
+            Case Else
+                Throw New Exception("Wrong File Name " & fileName & " :'(")
+        End Select
+
+        Return pdata
+
+    End Function
+End Class
+
